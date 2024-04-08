5549e95eec7b8524ddf8da0bd0b85f4dd6aefb7c
TettehSolomon <tettehsolomon106@gmail.com>
2024-04-08 08:49:01 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//Leap Year Checker: Create a function that takes a year as input and determines whether it is a leap year or not. Leap years are divisible by 4, but not by 100 unless they are also divisible by 400.
+function isLeap(year) {
+    // this function checks if the year is a leap year or not using the given conditions
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isLeap(2073)); //true
